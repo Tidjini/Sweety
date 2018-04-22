ba018a4f21e6ec53907fa064c9490fd03e55ee8d
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-22 15:03:06 +0100

    Add Outline Button

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import colors from "../assets/colors";
 import { Button } from "./components/common";
 import AppIcon from "../assets/icons/icon";
 
-import Wellcome from "./components/scenes/Wellcome";
+import Login from "./components/scenes/Login";
 
 const { width, height } = Dimensions.get("window");
 const background = require("../assets/images/back_01.jpg");
 
 export default class App extends Component {
   render() {
-    return <Wellcome />;
+    return <Login />;
   }
 }
---
+++ src/components/common/OutlineButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { TouchableOpacity, Text } from "react-native";
+
+const OutlineButton = ({ onPress, children }) => {
+  const { outlineButton } = styles;
+  return (
+    <TouchableOpacity style={outlineButton} onPress={onPress}>
+      {children}
+    </TouchableOpacity>
+  );
+};
+const styles = {
+  outlineButton: { alignItems: "center", padding: 10 }
+};
+
+export { OutlineButton };
